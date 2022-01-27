187a0a84058a123dfc0bda06cc72626d756dab79
mreza <purenexusno1@gmail.com>
2022-01-27 10:23:14 +0330

    update_jan.27_10.23

---
+++ P3-functionCalls/app.ts
@@ -0,0 +1,28 @@
+import { Lexter } from "./lexer.js";
+import readLine from 'node:readline'
+import { stdin, stdout } from 'process'
+import { Parser } from "./parser.js";
+import { Interpreter } from "./Interpreter.js";
+
+
+const rl = readLine.createInterface(stdin, stdout);
+const interpreter: Interpreter = new Interpreter(null);
+
+
+rl.on('line', (line) => {
+    if (line === 'exit') {
+        rl.close()
+        return;
+    }
+    const lexer: Lexter = new Lexter(line);
+    const tokens = lexer.tokenize();
+    console.log(tokens);
+    const parser: Parser = new Parser(tokens);
+    const parsRes = parser.parse();
+    // console.log(JSON.stringify(parsRes, undefined, 4));
+    interpreter.ast = parsRes;
+    console.log(interpreter.Solve());
+})
+
+
+
---
+++ P3-functionCalls/lexer.ts
@@ -0,0 +1,132 @@
+import { Keywords, Token, TokenType } from "./tokens.js";
+
+const DIGITS = '0123456789'
+const DIGITS_DOT = DIGITS + '.';
+const WHITE_SPACE = ' \t'
+const LETTERS = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+const LETTERS_DASH = LETTERS + '_'
+const LETTERS_DASH_DIGITS = LETTERS_DASH + DIGITS
+
+
+export class Lexter {
+
+    private readonly src: string;
+    private tokenList: Token[];
+    private currentPos: number = -1;
+    private currentChar: string;
+
+    constructor(src: string) {
+        this.src = src;
+        this.tokenList = [];
+        this.advance();
+    }
+
+    private advance(): void {
+        this.currentPos++;
+        if (this.currentPos < this.src.length) {
+            this.currentChar = this.src[this.currentPos];
+        } else {
+            this.currentChar = undefined;
+        }
+    }
+
+    private appendToken(token: Token) {
+        this.tokenList.push(token);
+    }
+
+    public tokenize(): Token[] {
+
+        while (this.currentChar) {
+            const char = this.currentChar;
+            if (char === '+') {
+                this.appendToken({ type: TokenType.TT_PLUS });
+                this.advance();
+            } else if (char === '-') {
+                this.appendToken({ type: TokenType.TT_MINUS });
+                this.advance();
+            } else if (char === '*') {
+                this.appendToken({ type: TokenType.TT_MULTY });
+                this.advance();
+            } else if (char === '/') {
+                this.appendToken({ type: TokenType.TT_DIV });
+                this.advance();
+            } else if (char === '=') {
+                this.appendToken({ type: TokenType.TT_EQ });
+                this.advance();
+            }
+            else if (char === '(') {
+                this.appendToken({ type: TokenType.TT_L_PARAN });
+                this.advance();
+            } else if (char === ')') {
+                this.appendToken({ type: TokenType.TT_R_PARAN });
+                this.advance();
+            } else if (DIGITS.indexOf(char) !== -1) {
+                this.getNumber();
+            } else if (LETTERS_DASH.indexOf(char) !== -1) {
+                this.get_ID_KW();
+            }
+            else if (WHITE_SPACE.indexOf(char) !== -1) {
+                this.advance();
+            }
+            else {
+                // TODO: implement error-system...
+                console.log(`Invalid token: ${char}`);
+                return null;
+            }
+        }
+
+
+
+
+
+        this.appendToken({ type: TokenType.TT_EOF });
+        return this.tokenList;
+    }
+
+
+    private get_ID_KW() {
+        let token_str = this.currentChar;
+        this.advance();
+
+        while (this.currentChar && (LETTERS_DASH_DIGITS.indexOf(this.currentChar) !== -1)) {
+            token_str += this.currentChar;
+            this.advance();
+        }
+
+        if (token_str in Keywords) {
+            this.appendToken({ type: TokenType.TT_KEYWORD, keword: Keywords[token_str] })
+        } else {
+            this.appendToken({ type: TokenType.TT_IDENTIFIER, value: token_str })
+        }
+
+    }
+
+
+    private getNumber() {
+        let token_str = this.currentChar;
+        let dots = 0;
+        this.advance();
+
+
+        while (this.currentChar && (DIGITS_DOT.indexOf(this.currentChar) !== -1)) {
+            token_str += this.currentChar;
+            if (this.currentChar === '.') {
+                dots++;
+            }
+            this.advance();
+        }
+
+        if (dots > 1) {
+            // TODO: implement error system...
+            console.log(`Invalid token: more than one dots in: ${token_str}`);
+            return;
+        }
+
+        if (dots === 1) {
+            this.appendToken({ type: TokenType.TT_FLOAT, value: Number(token_str) });
+        } else {
+            this.appendToken({ type: TokenType.TT_INT, value: Number(token_str) });
+        }
+
+    }
+}
